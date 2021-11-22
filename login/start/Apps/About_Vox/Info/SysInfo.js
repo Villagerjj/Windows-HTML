37c270b699fb375c85bbe3e168c263b164d2bfa2
Villagerjj <56765269+Villagerjj@users.noreply.github.com>
2021-11-22 16:36:38 -0500

    added MAJOR FILES

---
+++ login/start/Apps/About_Vox/Info/SysInfo.js
@@ -0,0 +1,19 @@
+   /* SystemInfo */
+
+ var Root = "Yes";
+
+ var Verson = "1.0";
+
+ var ComputerName = "Windows PC";
+
+/* UserInfo */
+
+ var UserName = "admin";
+
+ var PassProtected = "no";
+
+ /* Not obtained by About Vox */
+
+ var Pass = "0000";
+
+ var max = "4";
